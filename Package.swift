06546d68bbac8970f041ab45032ec5a92981021f
Morgan Lieberthal <morgan@jmorgan.org>
2016-10-30 14:27:49 -0600

    Add SimpleNet target to abstract away common networking code

--- Package.swift
+++ Package.swift
@@ -4,11 +4,12 @@ let package = Package(
     name: "VimChannelKit",
     targets: [
         Target(name: "LoggerAPI", dependencies: []),
-        Target(name: "Channel", dependencies: ["LoggerAPI"]),
+        Target(name: "SimpleNet", dependencies: []),
+        Target(name: "Channel", dependencies: ["LoggerAPI", "SimpleNet"]),
         Target(name: "example-channel", dependencies: ["Channel", "LoggerAPI"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
+        .Package(url: "https://github.com/baberthal/SwiftyJSON.git",
                  majorVersion: 3),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket.git",
                  majorVersion: 0, minor: 11)
