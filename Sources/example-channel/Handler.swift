36e56f50002baf1e8d4db233cd8b70a9b4a2910c
Morgan Lieberthal <morgan@jmorgan.org>
2016-10-31 01:19:57 -0600

    Update example to reflect newest changes

--- Sources/example-channel/Handler.swift
+++ Sources/example-channel/Handler.swift
@@ -8,25 +8,44 @@
 
 import Channel
 import LoggerAPI
+import SwiftyJSON
 
 class Handler: ChannelDelegate {
   /// The channel received a request.
   ///
+  /// To respond to a message, atopters of this protocol should do the following:
+  ///
+  /// ````
+  /// func channel(_ channel: Channel, didReceiveMessage message: Message) {
+  ///     // your logic here...
+  ///     let response: JSON = ... // create the response
+  ///     channel.respondTo(message: message, with: response)
+  /// }
+  ///
+  /// ````
+  ///
   /// - parameter channel: The channel that received the reqest (sender).
   /// - parameter message: The message that was received by the channel.
-  func channel(_ channel: Channel, didReceiveMessage message: Message) {
+  ///
+  /// - returns: an optional response body to send back to the client
+  ///
+  /// - seealso: `Channel.respondTo(message:with:)`
+  @discardableResult
+  public func channel(_ channel: Channel, didReceiveMessage message: Message) -> JSON? {
     Log.info("Channel: \(channel) didReceiveMessage: \(message)")
     
     guard let bodyString = message.body.string else {
       Log.error("Unable to get string from body of message")
-      return
+      return nil
     }
 
     debugPrint("body string: ", bodyString)
 
     if bodyString == "hello!" {
-      channel.respondTo(message: message, with: "got it!")
-    } 
+      return "got it!"
+    }
+
+    return nil
   }
 
   func channel(
