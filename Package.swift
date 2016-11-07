27859aa8837748af6a61cc2b78889a538e9cc63a
Morgan Lieberthal <morgan@jmorgan.org>
2016-11-07 13:31:05 -0700

    Add test dependencies

--- Package.swift
+++ Package.swift
@@ -4,15 +4,14 @@ let package = Package(
     name: "VimChannelKit",
     targets: [
         Target(name: "LoggerAPI", dependencies: []),
-        Target(name: "SimpleNet", dependencies: []),
-        Target(name: "Channel", dependencies: ["LoggerAPI", "SimpleNet"]),
+        Target(name: "Channel", dependencies: ["LoggerAPI"]),
         Target(name: "example-channel", dependencies: ["Channel", "LoggerAPI"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/baberthal/SwiftyJSON.git",
-                 majorVersion: 3),
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git",
-                 majorVersion: 0, minor: 11)
+        .Package(url: "https://github.com/baberthal/SwiftyJSON.git", majorVersion: 3),
+        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 11),
+        .Package(url: "../Quick", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 5)
     ],
     exclude: ["Makefile", "docs/*", "README.md"]
 )
