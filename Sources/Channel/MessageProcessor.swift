5f34bcd4e3abe524795af4e18b26ba84f0751eb6
Morgan Lieberthal <morgan@jmorgan.org>
2016-11-07 16:55:51 -0700

    Clean up MessageProcessor to reflect API changes

--- Sources/Channel/MessageProcessor.swift
+++ Sources/Channel/MessageProcessor.swift
@@ -12,6 +12,9 @@ import LoggerAPI
 import Socket
 import SwiftyJSON
 
+/// The `MessageProcessor` class is responsible for processing data that is available
+/// on a `ChannelBackend` and passing the processed data to the appropriate delegate for
+/// handling.
 public class MessageProcessor: DataProcessor {
   /// Default buffer size used for creating data buffers
   static let bufferSize = 2048
@@ -21,13 +24,13 @@ public class MessageProcessor: DataProcessor {
   /// The `ChannelDelegate` that will handle the message post-processing
   public weak var delegate: ChannelDelegate?
 
-  /// The `Channel` that the request came in on
-  public weak var channel: Channel!
-
   /// A flag that indicates that there is a request in progress
   public var inProgress = true
 
   // MARK: - Private Properties
+  
+  /// The `ChannelBackend` that the request came in on
+  weak var backend: ChannelBackend!
 
   /// The incoming request we are working with
   private var request = Message()
@@ -45,9 +48,9 @@ public class MessageProcessor: DataProcessor {
   ///
   /// - parameter channel: The channel the connection is taking place on
   /// - parameter using: The delegate to handle messages after processing
-  public init(channel: Channel, using delegate: ChannelDelegate!) {
+  init(backend: ChannelBackend, using delegate: ChannelDelegate!) {
     self.delegate = delegate
-    self.channel = channel
+    self.backend = backend
   }
 
   // MARK: - DataProcessor methods
@@ -83,7 +86,7 @@ public class MessageProcessor: DataProcessor {
   ///
   /// - parameter from: An NSData object containing the bytes to be written to the socket.
   public func write(from data: Data) {
-    channel.write(from: data)
+    backend.write(from: data)
   }
 
   /// Write a sequence of bytes in an array to the socket
@@ -91,17 +94,17 @@ public class MessageProcessor: DataProcessor {
   /// - parameter from: An UnsafeRawPointer to the sequence of bytes to be written to the socket.
   /// - parameter length: The number of bytes to write to the socket.
   public func write(from bytes: UnsafeRawPointer, length: Int) {
-    channel.write(from: bytes, length: length)
+    backend.write(from: bytes.assumingMemoryBound(to: UInt8.self), count: length)
   }
 
   /// Write a sequence of bytes, from an unsafe buffer pointer to the socket
   public func write(from buffer: UnsafeBufferPointer<UInt8>) {
-    channel.write(buffer: buffer)
+    backend.write(from: buffer)
   }
   
   /// Close the socket and MARK this handler as no longer in progress.
   public func close() {
-    channel.prepareShutdown()
+    backend.prepareToClose()
   }
 
   // MARK: - Private Helper Methods
@@ -139,19 +142,9 @@ public class MessageProcessor: DataProcessor {
     self.state = .complete
 
     DispatchQueue.global().async { [unowned self] in
-      if let res = self.delegate?.channel(self.channel, didReceiveMessage: self.request),
-             res != JSON.null {
-        let response = Message(id: self.request.id, body: res)
-
-        defer { self.state = .reset }
-
-        do {
-          let data = try response.rawData()
-          self.write(from: data)
-        } catch let error {
-          Log.error("Error: \(error)")
-        }
-      }
+      guard let channel = self.backend.channel else { return }
+      self.delegate?.channel(channel, didReceiveMessage: self.request)
+      defer { self.state = .reset }
     }
   }
 }
